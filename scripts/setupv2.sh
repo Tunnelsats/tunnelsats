a319a91b854552254021fd9e27f1c0e4d94d0710
ziggie <ziggie1984@protonmail.com>
2022-06-22 14:29:58 +0200

    some v2 changes :)

--- scripts/setupv2.sh
+++ scripts/setupv2.sh
@@ -112,6 +112,68 @@ fi
 
 sleep 2
 
+
+
+#Create Docker Tunnelsat Network which stays persistent over restarts
+ if [ $isDocker ]; then 
+
+  checkdockernetwork=$(docker network ls  2> /dev/null | grep -c "docker-tunnelsats")
+  #the subnet needs a bigger subnetmask (25) than the normal umbrel_mainet subnetmask of 24
+  #otherwise the network will not be chosen as the gateway for outside connection
+  dockersubnet="10.9.9.0/25"
+
+  if [ $checkdockernetwork -eq 0 ];
+    docker network create "docker-tunnelsats" --subnet $dockersubnet -o "com.docker.network.driver.mtu"="1420" &> /dev/null
+    if [ $? -eq 0 ];
+      echo "> docker-tunnelsats created successfully";echo
+    else 
+      echo "> failed to create docker-tunnelsats network";echo
+      exit 1
+    fi
+  else
+      echo "> docker-tunnelsats already created";echo
+  fi
+
+  #Clean Routing Tables from prior failed wg-quick starts
+  delrule1=$(ip rule | grep -c "from all lookup main suppress_prefixlength 0")
+  delrule2=$(ip rule | grep -c "from $dockersubnet lookup 51820")
+  for i in $( seq 1 $delrule1 )
+    do
+      ip rule del from all table  main suppress_prefixlength 0
+  done
+
+  for i in $( seq 1 $delrule2 )
+    do
+      ip rule del from $dockersubnet table 51820
+  done
+
+  #Flush any rules which are still present from failed interface starts
+  ip route flush table 51820
+
+else
+  #Delete Rules for non-docker setup
+  #Clean Routing Tables from prior failed wg-quick starts
+  delrule1=$(ip rule | grep -c "from all lookup main suppress_prefixlength 0")
+  delrule2=$(ip rule | grep -c "from all fwmark 0xdeadbeef lookup 51820")
+  for i in $( seq 1 $delrule1 )
+    do
+      ip rule del from all table  main suppress_prefixlength 0
+  done
+
+  for i in $( seq 1 $delrule2 )
+    do
+      ip rule del from all fwmark 0xdeadbeef table  51820
+  done
+
+   #Flush any rules which are still present from failed interface starts
+  ip route flush table 51820
+
+
+fi
+
+
+
+
 # edit tunnelsats.conf, add PostUp/Down rules
 # and copy to destination folder
 echo "Applying network rules to wireguard conf file..."
@@ -119,19 +181,16 @@ inputDocker="
 FwMark = 0x3333
 Table = off
 
-PostUp = docker network create \"docker-tunnelsats\" --subnet \"10.9.9.0/25\" -o \"com.docker.network.driver.mtu\"=\"1420\"
 PostUp = ip rule add from \$(docker network inspect \"docker-tunnelsats\" | grep Subnet | awk '{print \$2}' | sed 's/[\",]//g') table 51820;ip rule add from all table main suppress_prefixlength 0
 PostUp = ip route add blackhole default metric 3 table 51820;
 PostUp = ip route add default dev %i metric 2 table 51820
 PostUp = sysctl -w net.ipv4.conf.all.rp_filter=0
-PostUp = docker network connect \"docker-tunnelsats\" \$(docker ps | grep 9735 | awk '{ print \$18 }')
+PostUp = docker network connect \"docker-tunnelsats\" \$(docker ps --format 'table {{.Image}}\t{{.Names}}\t{{.Ports}}' | grep 9735 | awk '{print \$2}')
 
 PostDown = ip rule del from \$(docker network inspect \"docker-tunnelsats\" | grep Subnet | awk '{print \$2}' | sed 's/[\",]//g') table 51820
 PostDown = ip rule del from all table  main suppress_prefixlength 0
-PostDown = ip route del blackhole default metric 3 table 51820
-PostDown = sysctl -w net.ipv4.conf.all.rp_filter=1
-PostDown = docker network disconnect docker-tunnelsats 
-PostDown = docker network rm \"docker-tunnelsats\" \$(docker ps | grep 9735 | awk '{ print \$18 }')
+PostDown = ip route flush table 51820
+PostDown = docker network disconnect docker-tunnelsats \$(docker ps --format 'table {{.Image}}\t{{.Names}}\t{{.Ports}}' | grep 9735 | awk '{print \$2}')
 "
 inputNonDocker="
 FwMark = 0xdeadbeef
@@ -139,16 +198,21 @@ Table = off
 
 PostUp = ip rule add not from all fwmark 0xdeadbeef table 51820;ip rule add from all table main suppress_prefixlength 0
 PostUp = ip route add default dev %i table 51820;
+PostUp = sysctl -w net.ipv4.conf.all.rp_filter=0
 
+#Firewall nftable rules
 PostUp = nft add table inet %i
-PostUp = nft add chain inet %i raw '{type filter hook prerouting priority raw; policy accept;}'; nft add rule inet %i raw iifname != %i ip daddr 10.9.0.1 fib saddr type != local counter drop
 PostUp = nft add chain inet %i prerouting '{type filter hook prerouting priority mangle; policy accept;}'; nft add rule inet %i prerouting meta mark set ct mark
-PostUp = nft add chain inet %i mangle '{type route hook output priority mangle; policy accept;}'; nft add rule inet %i mangle meta cgroup 1118498 meta mark set 0xdeadbeef
-PostUp = nft add chain inet %i nat'{type nat hook postrouting priority srcnat; policy accept;}'; nft add rule inet %i nat oif %i ct mark 0xdeadbeef drop;nft add rule inet %i nat oif != "lo" ct mark 0xdeadbeef masquerade
+PostUp = nft add chain inet %i mangle '{type route hook output priority mangle; policy accept;}'; nft add rule inet %i mangle meta mark != 0x3333 meta cgroup 1118498 meta mark set 0xdeadbeef
+PostUp = nft add chain inet %i nat'{type nat hook postrouting priority srcnat; policy accept;}'; nft insert rule inet %i nat fib saddr type != local oif != %i ct mark 0xdeadbeef drop;nft add rule inet %i nat oif != "lo" ct mark 0xdeadbeef masquerade
 PostUp = nft add chain inet %i postroutingmangle'{type filter hook postrouting priority mangle; policy accept;}'; nft add rule inet %i postroutingmangle meta mark 0xdeadbeef ct mark set meta mark
+
 
 PostDown = nft delete table inet %i
 PostDown = ip rule del from all table  main suppress_prefixlength 0; ip rule del not from all fwmark 0xdeadbeef table 51820
+PostDown = ip route flush table 51820
+PostDown = sysctl -w net.ipv4.conf.all.rp_filter=1
+
 "
 
 directory=$(dirname -- $(readlink -fn -- "$0"))
@@ -181,109 +245,114 @@ fi
 sleep 2
 
 
-# setup lnd for splitting
-# create file
-echo "Creating lightning splitting.sh file in /etc/wireguard/..."
-echo "#!/bin/sh
-set -e
-dir_netcls=\"/sys/fs/cgroup/net_cls\"
-torsplitting=\"/sys/fs/cgroup/net_cls/splitted_processes\"
-modprobe cls_cgroup
-if [ ! -d \"\$dir_netcls\" ]; then
-  mkdir \$dir_netcls
-  mount -t cgroup -o net_cls none \$dir_netcls
-  echo \"> Successfully added cgroup net_cls subsystem\"
-fi
-if [ ! -d \"\$torsplitting\" ]; then
-  mkdir /sys/fs/cgroup/net_cls/splitted_processes
-  echo 1118498  > /sys/fs/cgroup/net_cls/splitted_processes/net_cls.classid
-  echo \"> Successfully added Mark for net_cls subsystem\"
-else
-  echo \"> Mark for net_cls subsystem already present\"
-fi
-# add Lightning pid(s) to cgroup
-pgrep -x lnd | xargs -I % sh -c 'echo % >> /sys/fs/cgroup/net_cls/splitted_processes/tasks' > /dev/null
-pgrep -x lightningd | xargs -I % sh -c 'echo % >> /sys/fs/cgroup/net_cls/splitted_processes/tasks' > /dev/null
+if [ ! $isDocker ]; then
 
 
-count=\$(cat /sys/fs/cgroup/net_cls/splitted_processes/tasks | wc -l)
-if [ \$count -eq 0 ];then
-  echo \"> ERR: no pids added to file\"
-  exit 1
-else
-  echo \"> \${count} Process(es) successfully excluded\"
-fi
-" > /etc/wireguard/splitting.sh
-if [ -f /etc/wireguard/splitting.sh ]; then
-  echo "> /etc/wireguard/splitting.sh created.";echo
-else
-  echo "> ERR: /etc/wireguard/splitting.sh was not created. Please check for errors.";
-  exit 1
-fi
-
-# run it once
-if [ -f /etc/wireguard/splitting.sh ];then
-    echo "> splitting.sh created, executing...";
-    # run
-    bash /etc/wireguard/splitting.sh
-    echo "> Split-tunneling successfully executed";echo
-else
-    echo "> ERR: splitting.sh execution failed";echo
-    exit 1
-fi
+    # setup lnd/clnfor splitting
+    # create file
+    echo "Creating lightning splitting.sh file in /etc/wireguard/..."
+    echo "#!/bin/sh
+    set -e
+    dir_netcls=\"/sys/fs/cgroup/net_cls\"
+    torsplitting=\"/sys/fs/cgroup/net_cls/splitted_processes\"
+    modprobe cls_cgroup
+    if [ ! -d \"\$dir_netcls\" ]; then
+      mkdir \$dir_netcls
+      mount -t cgroup -o net_cls none \$dir_netcls
+      echo \"> Successfully added cgroup net_cls subsystem\"
+    fi
+    if [ ! -d \"\$torsplitting\" ]; then
+      mkdir /sys/fs/cgroup/net_cls/splitted_processes
+      echo 1118498  > /sys/fs/cgroup/net_cls/splitted_processes/net_cls.classid
+      echo \"> Successfully added Mark for net_cls subsystem\"
+    else
+      echo \"> Mark for net_cls subsystem already present\"
+    fi
+    # add Lightning pid(s) to cgroup
+    pgrep -x lnd | xargs -I % sh -c 'echo % >> /sys/fs/cgroup/net_cls/splitted_processes/tasks' > /dev/null
+    pgrep -x lightningd | xargs -I % sh -c 'echo % >> /sys/fs/cgroup/net_cls/splitted_processes/tasks' > /dev/null
 
 
-# enable systemd service
-# create systemd file
-echo "Creating splitting systemd service..."
-# LND
-if [ ! -f /etc/systemd/system/splitting.service ] && [ -f /etc/systemd/system/lnd.service ]; then
-     echo "[Unit]
-Description=Splitting Lightning Traffic after Restart
-# Make sure it starts when lightning service is running (thats why restart settings are crucial here)
-Requires=lnd.service
-After=lnd.service
-StartLimitInterval=200
-StartLimitBurst=5
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/bin/bash /etc/wireguard/splitting.sh
-[Install]
-WantedBy=multi-user.target
-" > /etc/systemd/system/splitting.service
-elif  [ ! -f /etc/systemd/system/splitting.service ] && [ -f /etc/systemd/system/lightningd.service ]; then
-     echo "[Unit]
-Description=Splitting Lightning Traffic after Restart
-# Make sure it starts when lightning service is running (thats why restart settings are crucial here)
-Requires=lightningd.service
-After=lightningd.service
-StartLimitInterval=200
-StartLimitBurst=5
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/bin/bash /etc/wireguard/splitting.sh
-[Install]
-WantedBy=multi-user.target
-" > /etc/systemd/system/splitting.service
-fi
+    count=\$(cat /sys/fs/cgroup/net_cls/splitted_processes/tasks | wc -l)
+    if [ \$count -eq 0 ];then
+      echo \"> ERR: no pids added to file\"
+      exit 1
+    else
+      echo \"> \${count} Process(es) successfully excluded\"
+    fi
+    " > /etc/wireguard/splitting.sh
+    if [ -f /etc/wireguard/splitting.sh ]; then
+      echo "> /etc/wireguard/splitting.sh created.";echo
+    else
+      echo "> ERR: /etc/wireguard/splitting.sh was not created. Please check for errors.";
+      exit 1
+    fi
 
-# enable and start splitting.service
-if [ -f /etc/systemd/system/splitting.service ]; then
-  systemctl daemon-reload > /dev/null
-  if systemctl enable splitting.service > /dev/null &&
-     systemctl start splitting.service > /dev/null; then
-    echo "> splitting.service: systemd service enabled and started";echo
-  else
-    echo "> ERR: splitting.service could not be enabled or started. Please check for errors.";echo
-  fi
-else
-  echo "> ERR: splitting.service was not created. Please check for errors.";echo
-  exit 1
-fi
+    # run it once
+    if [ -f /etc/wireguard/splitting.sh ];then
+        echo "> splitting.sh created, executing...";
+        # run
+        bash /etc/wireguard/splitting.sh
+        echo "> Split-tunneling successfully executed";echo
+    else
+        echo "> ERR: splitting.sh execution failed";echo
+        exit 1
+    fi
 
-sleep 2
+
+    # enable systemd service
+    # create systemd file
+    echo "Creating splitting systemd service..."
+    # LND
+    if [ ! -f /etc/systemd/system/splitting.service ] && [ -f /etc/systemd/system/lnd.service ]; then
+        echo "[Unit]
+    Description=Splitting Lightning Traffic after Restart
+    # Make sure it starts when lightning service is running (thats why restart settings are crucial here)
+    Requires=lnd.service
+    After=lnd.service
+    StartLimitInterval=200
+    StartLimitBurst=5
+    [Service]
+    Type=oneshot
+    RemainAfterExit=yes
+    ExecStart=/usr/bin/bash /etc/wireguard/splitting.sh
+    [Install]
+    WantedBy=multi-user.target
+    " > /etc/systemd/system/splitting.service
+    elif  [ ! -f /etc/systemd/system/splitting.service ] && [ -f /etc/systemd/system/lightningd.service ]; then
+        echo "[Unit]
+    Description=Splitting Lightning Traffic after Restart
+    # Make sure it starts when lightning service is running (thats why restart settings are crucial here)
+    Requires=lightningd.service
+    After=lightningd.service
+    StartLimitInterval=200
+    StartLimitBurst=5
+    [Service]
+    Type=oneshot
+    RemainAfterExit=yes
+    ExecStart=/usr/bin/bash /etc/wireguard/splitting.sh
+    [Install]
+    WantedBy=multi-user.target
+    " > /etc/systemd/system/splitting.service
+    fi
+
+    # enable and start splitting.service
+    if [ -f /etc/systemd/system/splitting.service ]; then
+      systemctl daemon-reload > /dev/null
+      if systemctl enable splitting.service > /dev/null &&
+        systemctl start splitting.service > /dev/null; then
+        echo "> splitting.service: systemd service enabled and started";echo
+      else
+        echo "> ERR: splitting.service could not be enabled or started. Please check for errors.";echo
+      fi
+    else
+      echo "> ERR: splitting.service was not created. Please check for errors.";echo
+      exit 1
+    fi
+
+    sleep 2
+
+fi
 
 
 ## create and enable wireguard service
@@ -296,19 +365,36 @@ else
   echo "> ERR: wireguard service could not be enabled/started. Please check for errors.";echo
 fi
 
-
-#Check if tunnel works
-ipHome=$(curl --silent https://api.ipify.org)
-ipVPN=$(cgexec -g net_cls:splitted_processes curl --silent https://api.ipify.org)
-if [ "$ipHome" != "$ipVPN" ]; then
-    echo "> Tunnel is active
-    Your ISP external IP: ${ipHome} 
-    Your Tunnelsats external IP: ${ipVPN}";echo
+if [ ! $isDocker ]; then 
+  #Check if tunnel works
+  ipHome=$(curl --silent https://api.ipify.org)
+  ipVPN=$(cgexec -g net_cls:splitted_processes curl --silent https://api.ipify.org)
+  if [ "$ipHome" != "$ipVPN" ]; then
+      echo "> Tunnel is active
+      Your ISP external IP: ${ipHome} 
+      Your Tunnelsats external IP: ${ipVPN}";echo
+  else
+    echo "> ERR: Tunnelsats VPN Interface not successfully activated, check debug logs";echo
+    exit 1
+  fi
 else
-   echo "> ERR: Tunnelsats VPN Interface not successfully activated, check debug logs";echo
-   exit 1
-fi
 
+  docker pull appropriate/curl
+  if [ $? -ne 0 ]; then
+      echo "> Tunnel Verification not checked bc appropriate/curl not available on your system ";echo
+  else
+    ipHome=$(curl --silent https://api.ipify.org)
+    ipVPN=$(docker run -ti --rm --net=docker-tunnelsats appropriate/curl https://api.ipify.org &> /dev/null)
+  if [ "$ipHome" != "$ipVPN" ]; then
+      echo "> Tunnel is active
+      Your ISP external IP: ${ipHome} 
+      Your Tunnelsats external IP: ${ipVPN}";echo
+  else
+      echo "> ERR: Tunnelsats VPN Interface not successfully activated, check debug logs";echo
+      exit 1
+  fi
+
+fi
 
 ## UFW firewall configuration
 vpnExternalPort=$(grep "#VPNPort" /etc/wireguard/tunnelsatsv2.conf | awk '{ print $3 }')
