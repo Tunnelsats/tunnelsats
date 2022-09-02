b69aee2a3e19eb7061682e0321cc4c9ad742a252
blckbx <74455114+blckbx@users.noreply.github.com>
2022-09-02 14:47:17 +0200

    another try to fix popup

--- frontend/client/src/components/Popup.js
+++ frontend/client/src/components/Popup.js
@@ -1,9 +1,25 @@
 import React from "react";
+import {
+  Modal,
+  Button,
+} from "react-bootstrap";
 
 const Popup = (props) => {
   return (
-    <div className="popup-box">
-          <div className="box">{props.errorMessage}</div>
+    <div>
+        <Modal show={props.show} onHide={props.handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Something went wrong</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {props.errorMessage}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-warning" onClick={props.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>      
     </div>
   );
 };
