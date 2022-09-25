c4b1733aabb70ffad6ffbcef602aa22ebcba46f2
blckbx <74455114+blckbx@users.noreply.github.com>
2022-09-25 10:55:49 +0200

    rating points and result message

--- scripts/check.sh
+++ scripts/check.sh
@@ -7,7 +7,7 @@
 ##########UPDATE IF YOU MAKE A NEW RELEASE#############
 major=0
 minor=0
-patch=3
+patch=4
 
 # check if sudo
 if [ "$EUID" -ne 0 ]; then
@@ -26,6 +26,9 @@ echo -e "
 ###############################"
 echo
 
+# global const
+rating=0
+
 # check kernel version (min 5.10.102+ required)
 kernelMajor=$(uname -r | cut -d '.' -f1)
 kernelMinor=$(uname -r | cut -d '.' -f2)
@@ -41,9 +44,11 @@ if [[ $kernelMajor -ge 5 ]] &&
         [[ $kernelMinor -ge 11 ]]); then
     echo "> ✅ kernel version ok"
     echo
+    ((rating++))
 else
     echo "> ❌ kernel version 5.10.102+ required"
     echo
+    ((rating--))
 fi
 
 # requirement nftable version 0.9.6+
@@ -69,14 +74,17 @@ nftPatch=$(echo "${nftablesVersion}" | cut -d '.' -f3)
 if [[ $nftMajor -ge 1 ]]; then
     echo "> ✅ nftables version ok"
     echo
+    ((rating++))    
 # 0.9.6
 elif ([[ $nftMinor -ge 9 ]] && [[ $nftPatch -ge 6 ]] ||
     [[ $nftMinor -ge 10 ]]); then
     echo "> ✅ nftables version ok"
     echo
+    ((rating++))  
 else
     echo "> ❌ nftables version 0.9.6+ required"
     echo
+    ((rating--))
 fi
 
 # requirement: systemd.services or docker processes + related naming convention
@@ -84,9 +92,11 @@ echo "Looking for systemd services..."
 if [[ -f /etc/systemd/system/lnd.service ]]; then
     echo "> ✅ lnd.service found"
     echo
+    ((rating++))
 elif [[ -f /etc/systemd/system/lightningd.service ]]; then
     echo "> ✅ lightningd.service found"
     echo
+    ((rating++))
 else
     echo "> ❌ no systemd.services found, let's check for docker..."
     echo
@@ -95,11 +105,31 @@ else
     if [[ ${dockerProcess} == *lnd* ]]; then
         echo "> ✅ found a possible lnd container"
         echo
+        ((rating+=1))
     elif [[ ${dockerProcess} == *clightning* ]]; then
         echo "> ✅ found a possible cln container"
         echo
+        ((rating++))
     else
         echo "> ❌ no suitable containers found"
         echo
+        ((rating--))
     fi
+fi
+
+
+# result message
+# max points: 3
+if [ $rating -lt 0 ]; then
+    rating=0
+fi
+
+echo "Result:"
+echo "rating points: ${rating}"
+if [ $rating -le 0 ]; then
+    echo "> Sorry, your setup is likely not compatible with Tunnel Sats."
+elif [ $rating -gt 0 ] && [ $rating -lt 3 ]; then
+    echo "> Your setup is likely compatible with Tunnel Sats. Please check if missing requirements can be "
+elif [ $rating -eq 3 ]; then
+    echo "> Congrats! Your setup matches Tunnel Sats requirements perfectly!"
 fi
