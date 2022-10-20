37c74cb9c46d558ecf2198f753fe397b04afaf82
blckbx <74455114+blckbx@users.noreply.github.com>
2022-10-20 17:12:24 +0200

    remove ca1 from getSetver method

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -584,9 +584,9 @@ const getServer = (country) => {
   if (country == "oc") {
     server = process.env.IP_OCEANIA;
   }
-  if (country == "ca") {
-    server = process.env.IP_CANADA;
-  }
+  //if (country == "ca") {
+  //  server = process.env.IP_CANADA;
+  //}
   return server;
 };
 
