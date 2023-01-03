b011f34b43e5e94d36e93aa1964f05c56ba6b5d8
ziggie1984 <90319354+ziggie1984@users.noreply.github.com>
2023-01-03 21:21:04 +0100

    Create amboss-healt-plain-tor.sh

---
+++ scripts/amboss-healt-plain-tor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+URL="https://api.amboss.space/graphql"
+NOW=$(date -u +%Y-%m-%dT%H:%M:%S%z)
+SIGNATURE=$(/usr/local/bin/lncli signmessage "$NOW" | jq -r .signature)
+
+torPost=$(torify curl -s -X POST -H "Content-Type: application/json" -d '{
+        "query": "mutation healthCheck($signature: String!, $timestamp: String!) { healthCheck(signature: $signature, timestamp: $timestamp) }",
+                          "variables": {
+                                 "signature": "'"$SIGNATURE"'",
+                                 "timestamp": "'"$NOW"'"
+                          }
+                }' $URL)
+if [[ $torPost =~ "error" ]]; then
+    echo "> Amboss Health Ping failed"
+    exit 1
+fi
