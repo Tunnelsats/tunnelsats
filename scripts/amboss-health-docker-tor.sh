4988f067dab457567e6fbd8a81809059421755d9
ziggie1984 <90319354+ziggie1984@users.noreply.github.com>
2023-01-03 21:57:25 +0100

    Create amboss-health-docker-tor.sh

---
+++ scripts/amboss-health-docker-tor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eo pipefail
+
+URL="https://api.amboss.space/graphql"
+NOW=$(date -u +%Y-%m-%dT%H:%M:%S%z)
+LIGHTNINGCONTAINER=$(docker ps --format 'table {{.Image}} {{.Names}} {{.Ports}}' | grep 0.0.0.0:9735 | awk '{print $2}')
+SIGNATURE=$(docker exec $LIGHTNINGCONTAINER lncli signmessage "$NOW" | jq -r .signature)
+
+torPost=$(torify curl -s -X POST -H "Content-Type: application/json" -d '{
+                 "query": "mutation healthCheck($signature: String!, $timestamp: String!) { healthCheck(signature: $signature, timestamp: $timestamp) }",
+                          "variables": {
+                                 "signature": "'"$SIGNATURE"'",
+                                 "timestamp": "'"$NOW"'"
+                          }
+                }' $URL)
+if ! [[ $torPost =~ "true" ]]; then
+    echo "> Amboss Health Ping failed"
+    exit 1
+fi
