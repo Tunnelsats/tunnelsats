1602f6b53e893e140b157bb5796537d27b2dd4af
ziggie <ziggie1984@protonmail.com>
2023-01-03 21:03:37 +0100

    add tor backup

--- scripts/amboss-health-docker.sh
+++ scripts/amboss-health-docker.sh
@@ -7,10 +7,24 @@ NOW=$(date -u +%Y-%m-%dT%H:%M:%S%z)
 LIGHTNINGCONTAINER=$(docker ps --format 'table {{.Image}} {{.Names}} {{.Ports}}' | grep 0.0.0.0:9735 | awk '{print $2}')
 SIGNATURE=$(docker exec $LIGHTNINGCONTAINER lncli signmessage "$NOW" | jq -r .signature)
 
-docker run --rm --net=docker-tunnelsats curlimages/curl -s -X POST -H "Content-Type: application/json" -d '{
+clearnetPost=$(docker run --rm --net=docker-tunnelsats curlimages/curl -s -X POST -H "Content-Type: application/json" -d '{
   "query": "mutation healthCheck($signature: String!, $timestamp: String!) { healthCheck(signature: $signature, timestamp: $timestamp) }",
   "variables": {
    "signature": "'"$SIGNATURE"'",
     "timestamp": "'"$NOW"'"
   }
-}' $URL --output /dev/null
+}' $URL)
+
+if [[ $clearnetPost =~ "error" ]]; then
+  torPost=$(torify curl -s -X POST -H "Content-Type: application/json" -d '{
+                 "query": "mutation healthCheck($signature: String!, $timestamp: String!) { healthCheck(signature: $signature, timestamp: $timestamp) }",
+                          "variables": {
+                                 "signature": "'"$SIGNATURE"'",
+                                 "timestamp": "'"$NOW"'"
+                          }
+                }' $URL)
+  if [[ $torPost =~ "error" ]]; then
+    echo "> Amboss Health Ping failed"
+    exit 1
+  fi
+fi
